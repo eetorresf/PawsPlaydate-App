6b45415e2af71810d7b3e87746a8cf8f583685d0
Erika Torres Figueroa <torrespurple@gmail.com>
2023-01-24 12:40:03 -0500

    Added HomeViewController. Added Logout Button.

---
+++ PawsPlaydate-App/User/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  PawsPlaydate-App
+//
+//  Created by Erika Dey on 1/24/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeInLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logOutButton_Clicked(_ sender: Any) {
+    }
+    
+}
